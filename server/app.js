8a5fe69cdfe75e5aaaeafad444d1e881d85c5129
[libin9625] <1135909273@qq.com>
2018-03-08 11:46:29 +0800

    libin webpack run success

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.listen(3000,() => { 
+    console.log('app listening on port 3000.') 
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
